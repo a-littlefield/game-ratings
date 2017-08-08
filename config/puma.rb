c926ae80b39bbed36ef378e8625c357b7d9da6b2
Andrew Littlefield <andrew.ray.allan@gmail.com>
2017-08-08 09:19:54 -0600

    Setup of React on Rails

--- config/puma.rb
+++ config/puma.rb
@@ -29,6 +29,23 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # process behavior so workers use less memory. If you use this option
 # you need to make sure to reconnect any threads in the `on_worker_boot`
 # block.
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads threads_count, threads_count
+
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
+
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
+end
+
+
 #
 # preload_app!
 
